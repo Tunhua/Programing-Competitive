0021106b94a288fef688414043dae585d0b9a3f3
tunhua <36795552+Tunhua@users.noreply.github.com>
2018-05-21 12:18:45 +0900

    Create 3B.cpp

---
+++ 3B.cpp
@@ -0,0 +1,45 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+using ll = long long;
+const double pi = acos(-1);
+#define FOR(i,a,b) for (ll i=(a),__last_##i=(b);i<__last_##i;i++)
+#define RFOR(i,a,b) for (ll i=(b)-1,__last_##i=(a);i>=__last_##i;i--)
+#define REP(i,n) FOR(i,0,n)
+#define RREP(i,n) RFOR(i,0,n)
+#define __GET_MACRO3(_1, _2, _3, NAME, ...) NAME
+#define rep(...) __GET_MACRO3(__VA_ARGS__, FOR, REP)(__VA_ARGS__)
+#define rrep(...) __GET_MACRO3(__VA_ARGS__, RFOR, RREP)(__VA_ARGS__)
+template<typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+	REP(i, v.size()) { if (i)os << " "; os << v[i]; }return os;
+}
+template<typename T> ostream& operator<<(ostream& os, const vector<vector<T>>& v) {
+	REP(i, v.size()) { if (i)os << endl; os << v[i]; }return os;
+}
+
+const ll INF = 1LL << 60;
+ll MOD = 1000000007;
+ll _MOD = 1000000009;
+double EPS = 1e-10;
+
+int main() {
+	string s, p;
+
+	cin >> s >> p;
+
+	s = s + s;
+
+	REP(i, s.size()) {
+		REP(j, p.size()) {
+			if (p[j] != s[i + j]) {
+				break;
+			}
+			if (j == p.size() - 1) {
+				cout << "Yes" << endl;
+				return 0;
+			}
+		}
+	}
+
+	cout << "No" << endl;
+}
